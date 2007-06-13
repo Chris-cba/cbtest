ab70448f0808eb5b8ef5bef10e3fb105c67f71df
smarshall <smarshall@521c773d-b55a-d943-8648-67ffa5d26747>
2007-06-13 15:32:40 +0000

    Initial revision
    git-svn-id: svn://127.0.0.1/mai@65 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ trunk/install/maipkw.sql
@@ -0,0 +1,218 @@
+-----------------------------------------------------------------------------
+--   SCCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/mai/install/maipkw.sql-arc   2.0   Jun 13 2007 16:32:40   smarshall  $
+--       Module Name      : $Workfile:   maipkw.sql  $
+--       Date into SCCS   : $Date:   Jun 13 2007 16:32:40  $
+--       Date fetched Out : $Modtime:   Jun 13 2007 16:31:50  $
+--       SCCS Version     : $Revision:   2.0  $
+--       based on SCCS Version     : 1.3
+--
+-----------------------------------------------------------------------------
+-- Originally taken from '@(#)maipck.sql	1.22 11/27/02'
+-----------------------------------------------------------------------------
+-- Copyright (c) 2003 exor corporation.
+--
+-- This script creates all package wrapped bodies for the Maintenance Manager product.
+-- It should be executed during upgrades and new installations.
+-----------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------
+-- Mai package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'mai.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiReports package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'mai_reports.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiGIS package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'maigis.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiPBI package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'maipbi.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiSplit package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'maisplit.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiMerge package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'maimerge.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiRepl package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'mairepl.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiRecal package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'mairecal.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiClose package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'maiclose.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- PMSLayer package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'pmslayer.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- WWO package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'works_func.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- IntUtil package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'int_utility.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- InvCopy package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'inv_copy.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiWO package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+         '&terminator'||'pck'||'&terminator'||'maiwo.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- Interfaces package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+        '&terminator'||'pck'||'&terminator'||'interfaces.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- PInterface package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+        '&terminator'||'pck'||'&terminator'||'pinterface.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiFinM package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+        '&terminator'||'pck'||'&terminator'||'maifinm.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- Inv package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+        '&terminator'||'pck'||'&terminator'||'inv.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- Mai3863 package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+        '&terminator'||'pck'||'&terminator'||'mai3863.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiBudgets package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+        '&terminator'||'pck'||'&terminator'||'mai_budgets.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- Mai2325 package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+        '&terminator'||'pck'||'&terminator'||'mai2325.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
+-- MaiAudit package
+-----------------------------------------------------------------------------
+set define on
+select '&exor_base'||'mai'||'&terminator'||'admin'||
+        '&terminator'||'pck'||'&terminator'||'mai_audit.pkw' run_file
+from dual
+/
+start '&run_file'
+-----------------------------------------------------------------------------
---
+++ trunk/install/mairoles.sql
@@ -0,0 +1,205 @@
+-----------------------------------------------------------------------------
+--
+--   SCCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/mai/install/mairoles.sql-arc   2.0   Jun 13 2007 16:32:40   smarshall  $
+--       Module Name      : $Workfile:   mairoles.sql  $
+--       Date into SCCS   : $Date:   Jun 13 2007 16:32:40  $
+--       Date fetched Out : $Modtime:   Jun 13 2007 16:31:50  $
+--       SCCS Version     : $Revision:   2.0  $
+--       based on SCCS Version     : 1.2
+--
+-----------------------------------------------------------------------------
+--   Originally taken from  '@(#)mairoles.sql	1.7 09/28/00';
+--   STP roles are now PMS roles, and have been moved to PMS upgrade scripts
+-----------------------------------------------------------------------------
+
+rem --------------------------------------------------------------------------
+rem	Create a role for granting to the maintenance manager administrator.
+
+set feedback off
+
+PROMPT CREATE ROLE MAI_ADMIN;
+CREATE ROLE MAI_ADMIN;
+BEGIN
+   EXECUTE IMMEDIATE 'grant mai_admin to '||USER;
+   EXECUTE IMMEDIATE 'grant mai_admin to '||USER||' with admin option';
+END;
+/
+
+GRANT SELECT ANY TABLE TO MAI_ADMIN;
+GRANT INSERT ANY TABLE TO MAI_ADMIN;
+GRANT UPDATE ANY TABLE TO MAI_ADMIN;
+GRANT DELETE ANY TABLE TO MAI_ADMIN;
+GRANT LOCK ANY TABLE TO MAI_ADMIN;
+GRANT CREATE ANY TABLE TO MAI_ADMIN;
+GRANT CREATE ANY VIEW TO MAI_ADMIN;
+GRANT EXECUTE ANY PROCEDURE TO MAI_ADMIN;
+GRANT SELECT ANY SEQUENCE TO MAI_ADMIN;
+GRANT CREATE SESSION TO MAI_ADMIN;
+
+GRANT ALTER SESSION TO MAI_ADMIN;
+GRANT CREATE PUBLIC SYNONYM TO MAI_ADMIN;
+GRANT CREATE TRIGGER TO MAI_ADMIN;
+GRANT CREATE SEQUENCE TO MAI_ADMIN;
+GRANT CREATE ROLE TO MAI_ADMIN;
+GRANT CREATE SYNONYM TO MAI_ADMIN;
+GRANT CREATE PROCEDURE TO MAI_ADMIN;
+GRANT CREATE USER TO MAI_ADMIN;
+GRANT GRANT ANY PRIVILEGE TO MAI_ADMIN;
+GRANT GRANT ANY ROLE TO MAI_ADMIN;
+GRANT DROP PUBLIC SYNONYM TO MAI_ADMIN;
+GRANT DROP USER TO MAI_ADMIN;
+GRANT ALTER USER TO MAI_ADMIN;
+
+
+rem --------------------------------------------------------------------------
+rem	Create a role for granting to users who need update privileges.
+
+PROMPT CREATE ROLE MAI_USER;
+CREATE ROLE MAI_USER;
+BEGIN
+   EXECUTE IMMEDIATE 'grant mai_user to '||USER;
+   EXECUTE IMMEDIATE 'grant mai_user to '||USER||' with admin option';
+END;
+/
+
+GRANT SELECT ANY TABLE TO MAI_USER;
+GRANT INSERT ANY TABLE TO MAI_USER;
+GRANT UPDATE ANY TABLE TO MAI_USER;
+GRANT DELETE ANY TABLE TO MAI_USER;
+GRANT LOCK ANY TABLE TO MAI_USER;
+GRANT CREATE TABLE TO MAI_USER;
+GRANT CREATE VIEW  TO MAI_USER;
+GRANT SELECT ANY SEQUENCE TO MAI_USER;
+GRANT EXECUTE ANY PROCEDURE TO MAI_USER;
+GRANT CREATE SESSION TO MAI_USER;
+
+
+rem --------------------------------------------------------------------------
+rem	Create a role for granting to readonly users.
+
+PROMPT CREATE ROLE MAI_READONLY;
+CREATE ROLE MAI_READONLY;
+BEGIN
+   EXECUTE IMMEDIATE 'grant mai_readonly to '||USER;
+   EXECUTE IMMEDIATE 'grant mai_readonly to '||USER||' with admin option';
+END;
+/
+
+GRANT SELECT ANY TABLE TO MAI_READONLY;
+GRANT LOCK ANY TABLE TO MAI_READONLY;
+GRANT CREATE TABLE TO MAI_READONLY;
+GRANT CREATE VIEW  TO MAI_READONLY;
+GRANT SELECT ANY SEQUENCE TO MAI_READONLY;
+GRANT EXECUTE ANY PROCEDURE TO MAI_READONLY;
+GRANT CREATE SESSION TO MAI_READONLY;
+
+GRANT INSERT,UPDATE,DELETE ON BPR34A_TT TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON BPR34B_TT TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON BPR35A_TT TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON BPR35B_TT TO MAI_READONLY;
+GRANT        UPDATE        ON CONTRACTS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON INSURANCE_CLAIM_ACTIVITIES TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON INSURANCE_CLAIM_PARAMETERS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON NOTICES TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON NOTICE_DEFECTS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON PBI_QUERY TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON PBI_QUERY_ATTRIBS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON PBI_QUERY_TYPES TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON PBI_RESULTS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_CONTRACTS_LOCK TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_COMMENTS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_COMPSCHEMES TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_DEFBANDS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_DEFECTS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_DEFECT_LIST TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_DETTRTS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_REPORT_SECTIONS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_ROADCONS TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_STRIP_DATA TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_STRIP_HEADER TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TEMP_PMS4440_STRIP_LIST TO MAI_READONLY;
+GRANT INSERT,UPDATE,DELETE ON TMP_BPR2140 TO MAI_READONLY;
+GRANT        UPDATE        ON WORK_ORDERS TO MAI_READONLY;
+GRANT        UPDATE        ON WORK_ORDER_LINES TO MAI_READONLY;
+
+
+rem --------------------------------------------------------------------------
+rem     Create a role for granting to a MAI user allowed to go over budget.
+
+PROMPT CREATE ROLE OVER_BUDGET
+CREATE ROLE OVER_BUDGET;
+
+rem --------------------------------------------------------------------------
+rem	Create a role for granting to the structural projects administrator.
+
+prompt create role pms_admin;
+create role pms_admin;
+
+grant select any table to pms_admin;
+grant insert any table to pms_admin;
+grant update any table to pms_admin;
+grant delete any table to pms_admin;
+grant lock any table to pms_admin;
+grant create any table to pms_admin;
+grant create any view to pms_admin;
+grant execute any procedure to pms_admin;
+grant select any sequence to pms_admin;
+grant create session to pms_admin;
+
+
+rem --------------------------------------------------------------------------
+rem	Create a role for granting to users who need update privileges.
+
+prompt create role pms_user;
+create role pms_user;
+
+grant select any table to pms_user;
+grant insert any table to pms_user;
+grant update any table to pms_user;
+grant delete any table to pms_user;
+grant lock any table to pms_user;
+grant create table to pms_user;
+grant create view  to pms_user;
+grant select any sequence to pms_user;
+grant execute any procedure to pms_user;
+grant create session to pms_user;
+
+
+rem --------------------------------------------------------------------------
+rem	Create a role for granting to readonly users.
+
+prompt create role pms_readonly;
+create role pms_readonly;
+
+grant select any table to pms_readonly;
+grant lock any table to pms_readonly;
+grant create table to pms_readonly;
+grant create view  to pms_readonly;
+grant select any sequence to pms_readonly;
+grant execute any procedure to pms_readonly;
+grant create session to pms_readonly;
+
+grant insert,update,delete on pbi_query to pms_readonly;
+grant insert,update,delete on pbi_query_attribs to pms_readonly;
+grant insert,update,delete on pbi_query_types to pms_readonly;
+grant insert,update,delete on pbi_results to pms_readonly;
+grant insert,update,delete on temp_pms4440_comments to pms_readonly;
+grant insert,update,delete on temp_pms4440_compschemes to pms_readonly;
+grant insert,update,delete on temp_pms4440_defbands to pms_readonly;
+grant insert,update,delete on temp_pms4440_defects to pms_readonly;
+grant insert,update,delete on temp_pms4440_defect_list to pms_readonly;
+grant insert,update,delete on temp_pms4440_dettrts to pms_readonly;
+grant insert,update,delete on temp_pms4440_report_sections to pms_readonly;
+grant insert,update,delete on temp_pms4440_roadcons to pms_readonly;
+grant insert,update,delete on temp_pms4440_strip_data to pms_readonly;
+grant insert,update,delete on temp_pms4440_strip_header to pms_readonly;
+grant insert,update,delete on temp_pms4440_strip_list to pms_readonly;
+rem --------------------------------------------------------------------------
+
+rem ---------------------------------------------------------------------------
+rem These roles can now be assigned to Oracle users.
+
+REM End of command file
+REM
