324e7146129f5cfd45fbdf25323f0ace219d8e8d
smarshall <smarshall@521c773d-b55a-d943-8648-67ffa5d26747>
2007-06-27 09:15:24 +0000

    svnimporter: adding tags to revision 91
    git-svn-id: svn://127.0.0.1/mai@92 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/v4.0.4.0/admin/c/mai2222/mai2222.mk
@@ -0,0 +1,25 @@
+
+# ***********************************************************************
+#  ------------------------------- Comment ------------------------------
+#  SCCS Identifiers :-
+#       sccsid           : @(#)mai2222.mk	1.1 09/16/03
+#       Module Name      : mai2222.mk
+#       Date into SCCS   : 03/09/16 19:26:20
+#       Date fetched Out : 07/06/07 11:29:32
+#       SCCS Version     : 1.1
+#
+# ***********************************************************************
+#
+# To make this module :
+#   % make -f <makefile> <module> [USERID=<user>/<pass>]
+#
+
+PROC_FILES =  mai2222.pc dblogon.pc jobrun.pc sqlerr.pc
+C_FILES    =  catchsig.c unx_pipe.c
+H_FILES    =  rmms.h sqlext.h
+TARGET     =  mai2222  
+PL_SQL     =  full
+
+include ${PROD_HOME}/lib/proc_std.mk
+
+
