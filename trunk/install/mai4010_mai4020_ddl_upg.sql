7f7765a3d04666dae59ff8bd23885eb9841ba1d4
jwadsworth <jwadsworth@521c773d-b55a-d943-8648-67ffa5d26747>
2007-11-08 15:46:08 +0000

    Re-generated for 4020
    git-svn-id: svn://127.0.0.1/mai@309 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/install/mai4010_mai4020_ddl_upg.sql
+++ trunk/install/mai4010_mai4020_ddl_upg.sql
@@ -1,24 +1,15 @@
 ------------------------------------------------------------------
 -- mai4010_mai4020_ddl_upg.sql
 ------------------------------------------------------------------
-------------------------------------------------------------------
 
-------------------------------------------------------------------
--- PVCS Identifiers :-
+--   PVCS Identifiers :-
 --
--- pvcsid : $Header:   //vm_latest/archives/mai/install/mai4010_mai4020_ddl_upg.sql-arc   2.5   Nov 07 2007 09:17:46   jwadsworth  $
--- Module Name : $Workfile:   mai4010_mai4020_ddl_upg.sql  $
--- Date into PVCS : $Date:   Nov 07 2007 09:17:46  $
--- Date fetched Out : $Modtime:   Nov 07 2007 09:17:24  $
--- PVCS Version : $Revision:   2.5  $
--- Based on SCCS version : 
-
-
-------------------------------------------------------------------
---	Copyright (c) exor corporation ltd, 2007
-------------------------------------------------------------------
-
-
+--       pvcsid                 : $Header:   //vm_latest/archives/mai/install/mai4010_mai4020_ddl_upg.sql-arc   2.6   Nov 08 2007 15:46:08   jwadsworth  $
+--       Module Name      : $Workfile:   mai4010_mai4020_ddl_upg.sql  $
+--       Date into PVCS   : $Date:   Nov 08 2007 15:46:08  $
+--       Date fetched Out : $Modtime:   Nov 08 2007 15:45:56  $
+--       PVCS Version     : $Revision:   2.6  $
+--       Based on SCCS version : 
 ------------------------------------------------------------------
 
 ------------------------------------------------------------------
@@ -26,43 +17,6 @@ SET ECHO OFF
 SET LINESIZE 120
 SET HEADING OFF
 SET FEEDBACK OFF
-------------------------------------------------------------------
-
-
-------------------------------------------------------------------
-SET TERM ON
-PROMPT alter table BUDGETS
-SET TERM OFF
-
--- SSC  25-OCT-2007
--- 
--- DEVELOPMENT COMMENTS
--- Checking that BUD_RSE_HE_ID is nullable.
-------------------------------------------------------------------
-BEGIN
- EXECUTE IMMEDIATE ('alter table BUDGETS modify BUD_RSE_HE_ID null');
-EXCEPTION
-  WHEN others THEN 
-   Null;
-END; 
-
-
-------------------------------------------------------------------
-
-
-------------------------------------------------------------------
-SET TERM ON
-PROMPT Rebuild WOR_PK
-SET TERM OFF
-
--- AE  26-OCT-2007
--- 
--- DEVELOPMENT COMMENTS
--- Rebuild WOR_PK as a Primary key instead of Unique key
-------------------------------------------------------------------
-alter table work_orders drop constraint wor_pk;
-
-alter table work_orders add constraint WOR_PK primary key (wor_works_order_no) ENABLE;
 ------------------------------------------------------------------
 
 
@@ -130,6 +84,43 @@ WHERE '&run_4010' = 'N'
 SET FEEDBACK ON
 start &&run_file
 SET FEEDBACK OFF
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+SET TERM ON
+PROMPT alter table BUDGETS
+SET TERM OFF
+
+-- SSC  25-OCT-2007
+-- 
+-- DEVELOPMENT COMMENTS
+-- Checking that BUD_RSE_HE_ID is nullable.
+------------------------------------------------------------------
+BEGIN
+ EXECUTE IMMEDIATE ('alter table BUDGETS modify BUD_RSE_HE_ID null');
+EXCEPTION
+  WHEN others THEN 
+   Null;
+END; 
+
+
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+SET TERM ON
+PROMPT Rebuild WOR_PK
+SET TERM OFF
+
+-- AE  26-OCT-2007
+-- 
+-- DEVELOPMENT COMMENTS
+-- Rebuild WOR_PK as a Primary key instead of Unique key
+------------------------------------------------------------------
+alter table work_orders drop constraint wor_pk;
+
+alter table work_orders add constraint WOR_PK primary key (wor_works_order_no) ENABLE;
 ------------------------------------------------------------------
 
 
