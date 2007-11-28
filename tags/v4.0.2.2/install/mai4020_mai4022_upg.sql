93d57f2e9ce3cd5f0ae13dbd6b5c13451297d7da
jwadsworth <jwadsworth@521c773d-b55a-d943-8648-67ffa5d26747>
2007-11-28 08:56:40 +0000

    svnimporter: adding tags to revision 333
    git-svn-id: svn://127.0.0.1/mai@334 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/v4.0.2.2/install/mai4020_mai4022_upg.sql
@@ -0,0 +1,235 @@
+--
+-----------------------------------------------------------------------------
+--
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //vm_latest/archives/mai/install/mai4020_mai4022_upg.sql-arc   2.0   Nov 28 2007 08:56:40   jwadsworth  $
+--       Module Name      : $Workfile:   mai4020_mai4022_upg.sql  $
+--       Date into PVCS   : $Date:   Nov 28 2007 08:56:40  $
+--       Date fetched Out : $Modtime:   Nov 28 2007 08:56:16  $
+--       Version          : $Revision:   2.0  $
+--
+--   Product upgrade script
+--
+-----------------------------------------------------------------------------
+--	Copyright (c) exor corporation ltd, 2007
+-----------------------------------------------------------------------------
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+-- Grab date/time to append to log file names this is standard to all upgrade/install scripts
+undefine log_extension
+col         log_extension new_value log_extension noprint
+set term off
+select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+/
+set term on
+---------------------------------------------------------------------------------------------------
+-- Spool to Logfile
+define logfile1='mai4020_mai4022_1_&log_extension'
+define logfile2='mai4020_mai4022_2_&log_extension'
+spool &logfile1
+--get some db info
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product = 'MAI';
+---------------------------------------------------------------------------------------------------
+--                        ****************   CHECK(S)   *******************
+WHENEVER SQLERROR EXIT
+begin
+   hig2.pre_upgrade_check (p_product               => 'MAI'
+                          ,p_new_version           => '4.0.2.2'
+                          ,p_allowed_old_version_1 => '4.0.2.0'
+                          );
+END;
+/
+WHENEVER SQLERROR CONTINUE
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   DDL   *******************
+SET TERM ON
+PROMPT DDL Changes...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'mai'||'&terminator'||'install'||
+        '&terminator'||'mai4020_mai4022_ddl_upg.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--               ****************   RE-RUN INDEXES   *******************
+--SET TERM ON
+--PROMPT Re-running Indexes...
+--PROMPT Ignore errors resulting from index already existing...
+--SET TERM OFF
+--SET DEFINE ON
+--SELECT '&exor_base'||'mai'||'&terminator'||'install'||
+--        '&terminator'||'mai.ind' run_file
+--FROM dual
+--/
+--SET FEEDBACK ON
+--start &&run_file
+--SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--               ****************   RE-RUN CONSTRAINTS   *******************
+--SET TERM ON
+--PROMPT Re-running Constraints...
+--PROMPT Ignore errors resulting from constraint already existing...
+--SET TERM OFF
+--SET DEFINE ON
+--SELECT '&exor_base'||'mai'||'&terminator'||'install'||
+--        '&terminator'||'mai.con' run_file
+--FROM dual
+--/
+--SET FEEDBACK ON
+--start &&run_file
+--SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** VIEWS   ****************
+--SET TERM ON
+--PROMPT Views...
+--SET TERM OFF
+--SET DEFINE ON
+--SELECT '&exor_base'||'mai'||'&terminator'||'admin'||
+--        '&terminator'||'views'||'&terminator'||'maiviews.sql' run_file
+--FROM dual
+--/
+--SET FEEDBACK ON
+--start &&run_file
+--SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** TRIGGERS   ****************
+--
+--
+---------------------------------------------------------------------------------------------------
+--                  **************** PACKAGE HEADERS AND BODIES   ****************
+--
+--SET TERM ON
+--PROMPT Package Headers...
+--SET TERM OFF
+--SET DEFINE ON
+--SELECT '&exor_base'||'mai'||'&terminator'||'admin'||
+--       '&terminator'||'pck'||'&terminator'||'maipkh.sql' run_file
+--FROM dual
+--/
+--SET FEEDBACK ON
+--start &&run_file
+--SET FEEDBACK OFF
+--
+--
+--SET TERM ON
+--PROMPT Package Bodies...
+--SET TERM OFF
+--SET DEFINE ON
+--SELECT '&exor_base'||'mai'||'&terminator'||'admin'||
+--       '&terminator'||'pck'||'&terminator'||'maipkb.sql' run_file
+--FROM dual
+--/
+--SET FEEDBACK ON
+--start &&run_file
+--SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   COMPILE SCHEMA   *******************
+SET TERM ON
+Prompt Creating Compiling Schema Script...
+SET TERM OFF
+SPOOL OFF
+SET define ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'utl'||'&terminator'||'compile_schema.sql' run_file
+FROM dual
+/
+start '&run_file'
+spool &logfile2
+SET TERM ON
+start compile_all.sql
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   CONTEXT   *******************
+--The compile_all will have reset the user context so we must reinitialise it
+--
+SET FEEDBACK OFF
+SET TERM ON
+PROMPT Reinitialising Context...
+SET TERM OFF
+BEGIN
+  nm3context.initialise_context;
+  nm3user.instantiate_user;
+END;
+/
+---------------------------------------------------------------------------------------------------
+--                  ****************   METADATA  *******************
+SET TERM ON
+PROMPT Metadata Changes...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'mai'||'&terminator'||'install'||
+        '&terminator'||'mai4020_mai4022_metadata_upg.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   SYNONYMS   *******************
+SET TERM ON
+Prompt Creating Synonyms That Do Not Exist...
+SET TERM OFF
+EXECUTE nm3ddl.refresh_all_synonyms;
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   ROLES   *******************
+SET TERM ON
+Prompt Updating HIG_USER_ROLES...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+    '&terminator'||'hig_user_roles.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+Prompt Ensuring all users have HIG_USER role...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+    '&terminator'||'users_without_hig_user.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   VERSION NUMBER   *******************
+SET TERM ON
+Prompt Setting The Version Number...
+SET TERM OFF
+BEGIN
+      hig2.upgrade('MAI','mai4020_mai4022_upg.sql','Upgrade from 4.0.2.0 to 4.0.2.2','4.0.2.2');
+END;
+/
+COMMIT;
+SET HEADING OFF
+SELECT 'Product updated to version '||hpr_product||' ' ||hpr_version product_version
+FROM hig_products
+WHERE hpr_product = 'MAI';
+spool off
+exit
+---------------------------------------------------------------------------------------------------
+--                        ****************   END OF SCRIPT   *******************
