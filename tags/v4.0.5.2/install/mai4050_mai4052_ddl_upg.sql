406985cf5b17ab0fcce2d5bf8e113e2d095669ba
malexander <malexander@521c773d-b55a-d943-8648-67ffa5d26747>
2009-02-17 17:23:16 +0000

    svnimporter: adding tags to revision 699
    git-svn-id: svn://127.0.0.1/mai@700 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/v4.0.5.2/install/mai4050_mai4052_ddl_upg.sql
@@ -0,0 +1,48 @@
+------------------------------------------------------------------
+-- mai4050_mai4052_ddl_upg.sql
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+
+--
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //vm_latest/archives/mai/install/mai4050_mai4052_ddl_upg.sql-arc   3.2   Feb 17 2009 17:23:16   malexander  $
+--       Module Name      : $Workfile:   mai4050_mai4052_ddl_upg.sql  $
+--       Date into PVCS   : $Date:   Feb 17 2009 17:23:16  $
+--       Date fetched Out : $Modtime:   Feb 17 2009 17:21:50  $
+--       Version          : $Revision:   3.2  $
+--
+------------------------------------------------------------------
+--	Copyright (c) exor corporation ltd, 2008
+
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+SET TERM ON
+PROMPT WOL_REGISTER column
+SET TERM OFF
+
+------------------------------------------------------------------
+-- 
+-- DEVELOPMENT COMMENTS (MIKE HUITSON)
+-- Added As Part Of The MAI to TMA Interface Changes.
+-- 
+------------------------------------------------------------------
+ALTER TABLE work_order_lines
+ ADD (wol_register_flag  VARCHAR2(1) DEFAULT 'N' NOT NULL)
+/
+------------------------------------------------------------------
+
+
+
+------------------------------------------------------------------
+-- end of script 
+------------------------------------------------------------------
+
