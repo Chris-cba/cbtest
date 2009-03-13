fa9c918a36578cadbdff123b1b286668b5f411e4
gjohnson <gjohnson@521c773d-b55a-d943-8648-67ffa5d26747>
2009-03-13 15:13:56 +0000

    svnimporter: adding tags to revision 742
    git-svn-id: svn://127.0.0.1/mai@743 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/v4.0.5.2/admin/views/imf_mai_views.sql
@@ -0,0 +1,51 @@
+-------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //vm_latest/archives/mai/admin/views/imf_mai_views.sql-arc   3.0   Mar 13 2009 15:13:56   gjohnson  $
+--       Module Name      : $Workfile:   imf_mai_views.sql  $
+--       Date into PVCS   : $Date:   Mar 13 2009 15:13:56  $
+--       Date fetched Out : $Modtime:   Mar 13 2009 15:12:10  $
+--       Version          : $Revision:   3.0  $
+--       Based on SCCS version : 
+-------------------------------------------------------------------------
+--
+SET echo OFF
+SET term OFF
+--
+COL run_file new_value run_file noprint
+--
+-------------------------------------------------------------------------
+--
+--SET TERM ON
+--PROMPT view_name
+--SET TERM OFF
+--SET DEFINE ON
+--SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'view_name.vw' run_file
+--FROM dual
+--/
+--START '&run_file'
+--
+-------------------------------------------------------------------------
+--
+
+
+
+
+
+
+
+
+
+
+--
+-------------------------------------------------------------------------
+--
+-- new views above this
+--
+-------------------------------------------------------------------------
+--
+
+set term on
+
+
+
