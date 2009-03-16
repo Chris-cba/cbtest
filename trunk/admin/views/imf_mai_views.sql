c50e9d311856f13e9e597145c7ac8f181f43ae4a
drawat <drawat@521c773d-b55a-d943-8648-67ffa5d26747>
2009-03-16 16:09:42 +0000

    Initial version for Information Manager.
    git-svn-id: svn://127.0.0.1/mai@747 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/admin/views/imf_mai_views.sql
+++ trunk/admin/views/imf_mai_views.sql
@@ -1,11 +1,11 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/mai/admin/views/imf_mai_views.sql-arc   3.0   Mar 13 2009 15:13:56   gjohnson  $
+--       PVCS id          : $Header:   //vm_latest/archives/mai/admin/views/imf_mai_views.sql-arc   3.1   Mar 16 2009 16:09:42   drawat  $
 --       Module Name      : $Workfile:   imf_mai_views.sql  $
---       Date into PVCS   : $Date:   Mar 13 2009 15:13:56  $
---       Date fetched Out : $Modtime:   Mar 13 2009 15:12:10  $
---       Version          : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Mar 16 2009 16:09:42  $
+--       Date fetched Out : $Modtime:   Mar 16 2009 14:21:14  $
+--       Version          : $Revision:   3.1  $
 --       Based on SCCS version : 
 -------------------------------------------------------------------------
 --
@@ -16,27 +16,58 @@ COL run_file new_value run_file noprint
 --
 -------------------------------------------------------------------------
 --
---SET TERM ON
---PROMPT view_name
---SET TERM OFF
---SET DEFINE ON
---SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'view_name.vw' run_file
---FROM dual
---/
---START '&run_file'
+SET TERM ON
+PROMPT view_name
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_mai_bill_of_quantities.vw' run_file
+FROM dual
+/
+START '&run_file'
 --
 -------------------------------------------------------------------------
 --
-
-
-
-
-
-
-
-
-
-
+SET TERM ON
+PROMPT view_name
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_mai_inspections.vw' run_file
+FROM dual
+/
+START '&run_file'
+--
+-------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT view_name
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_mai_repairs.vw' run_file
+FROM dual
+/
+START '&run_file'
+--
+-------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT view_name
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_mai_defects.vw' run_file
+FROM dual
+/
+START '&run_file'
+--
+-------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT view_name
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_mai_defect_repairs.vw' run_file
+FROM dual
+/
+START '&run_file'
 --
 -------------------------------------------------------------------------
 --
