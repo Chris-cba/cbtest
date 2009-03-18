0594c317d6b6840801cc4a738c976e72860f7d84
smarshall <smarshall@521c773d-b55a-d943-8648-67ffa5d26747>
2009-03-18 14:13:44 +0000

    Added imf_mai_work_orders.vw and imf_work_worder_lines.vw
    git-svn-id: svn://127.0.0.1/mai@752 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/admin/views/imf_mai_views.sql
+++ trunk/admin/views/imf_mai_views.sql
@@ -1,11 +1,11 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/mai/admin/views/imf_mai_views.sql-arc   3.1   Mar 16 2009 16:09:42   drawat  $
+--       PVCS id          : $Header:   //vm_latest/archives/mai/admin/views/imf_mai_views.sql-arc   3.2   Mar 18 2009 14:13:44   smarshall  $
 --       Module Name      : $Workfile:   imf_mai_views.sql  $
---       Date into PVCS   : $Date:   Mar 16 2009 16:09:42  $
---       Date fetched Out : $Modtime:   Mar 16 2009 14:21:14  $
---       Version          : $Revision:   3.1  $
+--       Date into PVCS   : $Date:   Mar 18 2009 14:13:44  $
+--       Date fetched Out : $Modtime:   Mar 18 2009 14:13:14  $
+--       Version          : $Revision:   3.2  $
 --       Based on SCCS version : 
 -------------------------------------------------------------------------
 --
@@ -65,6 +65,28 @@ PROMPT view_name
 SET TERM OFF
 SET DEFINE ON
 SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_mai_defect_repairs.vw' run_file
+FROM dual
+/
+START '&run_file'
+--
+-------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT view_name
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_mai_work_orders.vw' run_file
+FROM dual
+/
+START '&run_file'
+--
+-------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT view_name
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_mai_work_order_lines.vw' run_file
 FROM dual
 /
 START '&run_file'
