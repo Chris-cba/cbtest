472f021dafb7738f8e2c233cadf05d3f49481c2b
drawat <drawat@521c773d-b55a-d943-8648-67ffa5d26747>
2009-03-18 15:29:58 +0000

    Added imf_mai_budgets.vw
    git-svn-id: svn://127.0.0.1/mai@758 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/admin/views/imf_mai_views.sql
+++ trunk/admin/views/imf_mai_views.sql
@@ -1,11 +1,11 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/mai/admin/views/imf_mai_views.sql-arc   3.2   Mar 18 2009 14:13:44   smarshall  $
+--       PVCS id          : $Header:   //vm_latest/archives/mai/admin/views/imf_mai_views.sql-arc   3.3   Mar 18 2009 15:29:58   drawat  $
 --       Module Name      : $Workfile:   imf_mai_views.sql  $
---       Date into PVCS   : $Date:   Mar 18 2009 14:13:44  $
---       Date fetched Out : $Modtime:   Mar 18 2009 14:13:14  $
---       Version          : $Revision:   3.2  $
+--       Date into PVCS   : $Date:   Mar 18 2009 15:29:58  $
+--       Date fetched Out : $Modtime:   Mar 18 2009 15:29:14  $
+--       Version          : $Revision:   3.3  $
 --       Based on SCCS version : 
 -------------------------------------------------------------------------
 --
@@ -87,6 +87,16 @@ PROMPT view_name
 SET TERM OFF
 SET DEFINE ON
 SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_mai_work_order_lines.vw' run_file
+FROM dual
+/
+START '&run_file'
+-------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT view_name
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'mai'||'&terminator'||'admin'||'&terminator'||'views'||'&terminator'||'imf_mai_budgets.vw' run_file
 FROM dual
 /
 START '&run_file'
