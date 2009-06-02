685f012857808af3d10c47b71e241c502f7d26b9
kdawson <kdawson@521c773d-b55a-d943-8648-67ffa5d26747>
2009-06-02 15:16:16 +0000

    Log 716904 & Log 719542 changes for NE_ID and RSE_DESCR
    git-svn-id: svn://127.0.0.1/mai@869 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/admin/c/mai2110c/mai2110_tab_cre.sql
+++ trunk/admin/c/mai2110c/mai2110_tab_cre.sql
@@ -1,6 +1,6 @@
 rem "@(#)mai2110_tab_cre.sql	1.1 09/08/03"
 create table hhinv_temp_7883
-           (he_id number(8), max_chain number(6))
+           (he_id number(9), max_chain number(6))
 /
 create table road_sect_ora as select * from road_segments_all where rownum < 1
 /
