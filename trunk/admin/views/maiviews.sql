9f05607981efc59fe4a9d8ff997c0b2ebf84307d
gjohnson <gjohnson@521c773d-b55a-d943-8648-67ffa5d26747>
2009-11-04 14:24:40 +0000

    Amended view definition
    git-svn-id: svn://127.0.0.1/mai@1168 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/admin/views/maiviews.sql
+++ trunk/admin/views/maiviews.sql
@@ -6,11 +6,11 @@ REM **************************************************************************
 --
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //vm_latest/archives/mai/admin/views/maiviews.sql-arc   2.6   Jul 16 2009 18:33:36   mhuitson  $
+--       sccsid           : $Header:   //vm_latest/archives/mai/admin/views/maiviews.sql-arc   2.7   Nov 04 2009 14:24:40   gjohnson  $
 --       Module Name      : $Workfile:   maiviews.sql  $
---       Date into SCCS   : $Date:   Jul 16 2009 18:33:36  $
---       Date fetched Out : $Modtime:   Jul 16 2009 18:29:06  $
---       SCCS Version     : $Revision:   2.6  $
+--       Date into SCCS   : $Date:   Nov 04 2009 14:24:40  $
+--       Date fetched Out : $Modtime:   Nov 03 2009 13:37:14  $
+--       SCCS Version     : $Revision:   2.7  $
 --       Based on 
 --
 -----------------------------------------------------------------------------
@@ -354,12 +354,15 @@ CREATE OR REPLACE FORCE VIEW v_mai3806_def(are_date_work_done
                                           ,rep_date_due
                                           ,rep_date_completed
                                           ,rep_descr
-                                          ,rep_tre_treat_code) AS
+                                          ,rep_tre_treat_code
+) AS
 SELECT /*+ FIRST_ROWS_N */
        are_date_work_done
       ,are_batch_id
       ,are_initiation_type
-      ,hus_initials
+      ,(select hus_initials
+        from   hig_users
+        where  are_peo_person_id_actioned = hus_user_id) hus_initials 
       ,are_maint_insp_flag
       ,ne_admin_unit
       ,are_st_chain
@@ -419,26 +422,48 @@ SELECT /*+ FIRST_ROWS_N */
       ,rep_date_completed
       ,rep_descr
       ,rep_tre_treat_code
-  FROM hig_users hus
-      ,nm_elements
+  FROM nm_elements
       ,repairs
       ,defects def
       ,activities_report
  WHERE are_report_id = def_are_report_id
-   AND are_peo_person_id_actioned = hus.hus_user_id(+)
    AND def_defect_id = rep_def_defect_id
    AND def_rse_he_id = ne_id
 /
 ------------------------------------------------------------------------------
 prompt Creating View V_MAI3806_DEF2
 
-CREATE OR REPLACE FORCE VIEW V_MAI3806_DEF2
-(WORKS_ORDER_NO, WOL_DATE_CREATED, WOR_EST_COMPLETE, WOL_DATE_COMPLETE, WOL_STATUS_CODE,
- WOL_DATE_PAID, WOL_CNP_ID, WOL_WORK_SHEET_NO, WOL_CHECK_ID, WOL_CHECK_DATE,
- WOR_CON_ID, WOR_DATE_CLOSED, HCO_MEANING, DEF_DEFECT_ID, REP_DESCR,
- REP_TRE_TREAT_CODE, REP_DATE_DUE, REP_DATE_COMPLETED, REP_DEF_DEFECT_ID, REP_SUPERSEDED_FLAG,
- REP_LOCAL_DATE_DUE, REP_OLD_DUE_DATE, REP_CREATED_DATE, REP_LAST_UPDATED_DATE, REP_ACTION_CAT,
- REP_ATV_ACTY_AREA_CODE, REP_COMPLETED_HRS, REP_COMPLETED_MINS, REP_RSE_HE_ID)
+CREATE OR REPLACE FORCE VIEW v_mai3806_def2
+( works_order_no
+, wol_date_created
+, wor_est_complete
+, wol_date_complete
+, wol_status_code
+, wol_date_paid
+, wol_cnp_id
+, wol_work_sheet_no
+, wol_check_id
+, wol_check_date
+, wor_con_id
+, wor_date_closed
+, wor_date_confirmed
+, hco_meaning
+, def_defect_id
+, rep_descr
+, rep_tre_treat_code
+, rep_date_due
+, rep_date_completed
+, rep_def_defect_id
+, rep_superseded_flag
+, rep_local_date_due
+, rep_old_due_date
+, rep_created_date
+, rep_last_updated_date
+, rep_action_cat
+, rep_atv_acty_area_code
+, rep_completed_hrs
+, rep_completed_mins
+, rep_rse_he_id)
 AS
 SELECT /*+ FIRST_ROWS_N */
        wol.wol_works_order_no
@@ -453,7 +478,12 @@ SELECT /*+ FIRST_ROWS_N */
       ,wol.wol_check_date
       ,wor.wor_con_id
       ,wor.wor_date_closed
-      ,hco.hco_meaning
+      ,wor.wor_date_confirmed
+      ,(select hco_meaning
+        from   hig_codes
+        where  hco_domain = 'CHECK_RESULT'
+         AND   hco_code = wol.wol_check_result
+        ) hco_meaning 
       ,rep.rep_def_defect_id
       ,rep.rep_descr
       ,rep.rep_tre_treat_code
@@ -471,14 +501,11 @@ SELECT /*+ FIRST_ROWS_N */
       ,rep.rep_completed_mins
       ,rep.rep_rse_he_id
   FROM repairs rep
-      ,hig_codes hco
       ,work_order_lines wol
       ,work_orders wor
  WHERE wor.wor_works_order_no(+) = wol.wol_works_order_no
    AND wol.wol_def_defect_id(+) = rep.rep_def_defect_id
    AND wol.wol_rep_action_cat(+) = rep.rep_action_cat
-   AND wol.wol_check_result = hco.hco_code(+)
-   AND hco.hco_domain(+) = 'CHECK_RESULT'
  ORDER
     BY wol.wol_id desc
 /
