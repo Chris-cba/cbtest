5bb01fc75721ac3fa4d2ede9aeef243c1258d51e
lsorathia <lsorathia@521c773d-b55a-d943-8648-67ffa5d26747>
2010-03-19 09:18:38 +0000

    Task 0109302 Application hanging when running Blackburn Work Order Extract
    git-svn-id: svn://127.0.0.1/mai@1343 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/10g_bin/mai3859.sql
+++ trunk/10g_bin/mai3859.sql
@@ -1,34 +1,35 @@
 -----------------------------------------------------------------------------
 --
---   SCCS Identifiers :-
+--   PVCS Identifiers :-
 --
---       sccsid           : @(#)mai3859.sql	1.1 09/24/03
---       Module Name      : mai3859.sql
---       Date into SCCS   : 03/09/24 11:56:24
---       Date fetched Out : 07/06/07 11:18:28
---       SCCS Version     : 1.1
+--       sccsid           : $Header:   //vm_latest/archives/mai/10g_bin/mai3859.sql-arc   4.1   Mar 19 2010 09:18:38   lsorathia  $
+--       Module Name      : $Workfile:   mai3859.sql  $
+--       Date into PVCS   : $Date:   Mar 19 2010 09:18:38  $
+--       Date fetched Out : $Modtime:   Mar 19 2010 09:03:32  $
+--       PVCS Version     : $Revision:   4.1  $
+--       Based on SCCS Version     : 1.1
 --
 -----------------------------------------------------------------------------
-rem  Date   : March 1999
-rem  Author : H.Buckley
-rem  Descr  : This program will enable the user to create specified output
-rem           file formats from the standard exor interface extraction
-rem           programs.
-rem
+
 set serveroutput on
 set ver off
 set feed off
 
 prompt
 
+exec HIGGRIRP.write_gri_spool('&1','Highways by Exor');
 prompt Highways by Exor
+exec HIGGRIRP.write_gri_spool('&1','================');
 prompt ================
 prompt
 prompt 
+exec HIGGRIRP.write_gri_spool('&1','Blackburn File Structure Interface Program');
 prompt Blackburn File Structure Interface Program
+exec HIGGRIRP.write_gri_spool('&1','');
 prompt
+exec HIGGRIRP.write_gri_spool('&1','Working ....');
 prompt Working ....
-prompt
+exec HIGGRIRP.write_gri_spool('&1','');
 prompt
 declare
   l_contractor_id	org_units.oun_contractor_id%type;
@@ -38,9 +39,27 @@ declare
 begin
   dbms_output.disable;
   Blackburn.main(&1);
+  HIGGRIRP.write_gri_spool(&1,'Info: File  Created');
+  dbms_output.put_line('Info: File Created');
   dbms_output.enable(300);
 end;
 /
-prompt
-pause Press Return to exit;
+exec HIGGRIRP.write_gri_spool('&1','Successfully Completed');
+
+set define on
+set term off
+set head off
+
+spool run_file.sql
+select 'Prompt Press Return to exit '||chr(10)||' pause'
+from gri_report_runs
+where grr_job_id = '&1'
+and grr_mode != 'WEB'
+/
+
+set term on
+set define off
+spool off
+start run_file
+
 exit;
