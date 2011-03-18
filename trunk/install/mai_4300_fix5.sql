febea11d097098184f9190437b038001958a5955
Mike.Alexander <Mike.Alexander@521c773d-b55a-d943-8648-67ffa5d26747>
2011-03-18 11:33:40 +0000

    Initial revision
    git-svn-id: svn://127.0.0.1/mai@2241 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ trunk/install/mai_4300_fix5.sql
@@ -0,0 +1,150 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/mai/install/mai_4300_fix5.sql-arc   3.0   Mar 18 2011 11:33:40   Mike.Alexander  $
+--       Module Name      : $Workfile:   mai_4300_fix5.sql  $
+--       Date into PVCS   : $Date:   Mar 18 2011 11:33:40  $
+--       Date fetched Out : $Modtime:   Mar 18 2011 11:33:18  $
+--       PVCS Version     : $Revision:   3.0  $
+--
+--------------------------------------------------------------------------------
+--   Copyright (c) exor corporation ltd, 2011
+--------------------------------------------------------------------------------
+--
+set echo off
+set linesize 120
+set heading off
+set feedback off
+--
+-- Grab date/time to append to log file name
+--
+undefine log_extension
+col      log_extension new_value log_extension noprint
+set term off
+select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+/
+set term on
+--
+--------------------------------------------------------------------------------
+--
+--
+-- Spool to Logfile
+--
+define logfile1='mai_4300_fix5_1_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+select 'Fix Date ' || to_char(sysdate, 'DD-MON-YYYY HH24:MM:SS') from dual;
+
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+--
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET','MAI');
+
+WHENEVER SQLERROR EXIT
+
+DECLARE
+ CURSOR c1 IS
+ SELECT DECODE (COUNT(*), 3, 'Y', NULL) FIX_1_TO_3_APPLIED
+ FROM  (SELECT 'FIX1'
+        FROM   hig_upgrades
+        WHERE  hup_product = 'MAI'
+        AND    remarks     = 'MAI 4300 FIX 1'
+        UNION
+        SELECT 'FIX2'
+        FROM   hig_upgrades
+        WHERE  hup_product = 'MAI'
+        AND    remarks     = 'MAI 4300 FIX 2' 
+        UNION
+        SELECT 'FIX3'
+        FROM   hig_upgrades
+        WHERE  hup_product = 'MAI'
+        AND    remarks     = 'MAI 4300 FIX 3'
+        UNION
+        SELECT 'FIX4'
+        FROM   hig_upgrades
+        WHERE  hup_product = 'MAI'
+        AND    remarks     = 'MAI 4300 FIX 4'
+       );
+ l_dummy VARCHAR2(1);
+BEGIN
+  --
+  -- Check that the user isn't sys or system
+  --
+  IF USER IN ('SYS','SYSTEM')
+  THEN
+    RAISE_APPLICATION_ERROR(-20000,'You cannot install this product as ' || USER);
+  END IF;
+
+  --
+  -- Check that MAI has been installed @ v4.3.0.0
+  --
+  hig2.product_exists_at_version (p_product        => 'MAI'
+                                 ,p_VERSION        => '4.3.0.0'
+                                 );
+
+ OPEN c1;
+ FETCH c1 INTO l_dummy;
+ CLOSE c1;
+
+ IF l_dummy IS NULL THEN
+   RAISE_APPLICATION_ERROR(-20001,'"Maintenance Manager 4300 Fixes 1, 2, 3 and 4" must be applied before proceeding - contact exor support for further information');
+ END IF;
+
+END;
+/
+WHENEVER SQLERROR CONTINUE
+--
+--
+--------------------------------------------------------------------------------
+-- Package Bodies
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT mai_inspection_api.pkw
+SET TERM OFF
+--
+SET FEEDBACK ON
+start mai_inspection_api.pkw
+SET FEEDBACK OFF
+--
+--
+SET TERM ON 
+PROMPT mai_inspection_loader.pkw
+SET TERM OFF
+--
+SET FEEDBACK ON
+start mai_inspection_loader.pkw
+SET FEEDBACK OFF
+--
+--
+--------------------------------------------------------------------------------
+-- Metadata
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT mai_4300_fix5_metadata_upg.sql
+SET TERM OFF
+--
+SET FEEDBACK ON
+start mai_4300_fix5_metadata_upg.sql
+SET FEEDBACK OFF
+--
+--------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT log_mai_4300_fix5.sql 
+--
+SET FEEDBACK ON
+start log_mai_4300_fix5.sql
+SET FEEDBACK OFF
+SPOOL OFF
+EXIT
+--
+--
