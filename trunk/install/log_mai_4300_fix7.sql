78d8f3f31871492b94f49e0cbdc4625b7af26887
Mike.Alexander <Mike.Alexander@521c773d-b55a-d943-8648-67ffa5d26747>
2011-08-12 13:25:02 +0000

    Initial revision
    git-svn-id: svn://127.0.0.1/mai@2406 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ trunk/install/log_mai_4300_fix7.sql
@@ -0,0 +1,13 @@
+BEGIN
+--
+  hig2.upgrade(p_product        => 'MAI'
+              ,p_upgrade_script => 'log_mai_4300_fix7.sql'
+              ,p_remarks        => 'MAI 4300 FIX 7'
+              ,p_to_version     => Null);
+--
+  commit;
+--
+EXCEPTION
+  WHEN others THEN Null;
+END;
+/
