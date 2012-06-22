12664753d5ab4f0e865ce1cc7cf792d6528dc3d7
Steve.Cooper <Steve.Cooper@521c773d-b55a-d943-8648-67ffa5d26747>
2012-06-22 08:49:26 +0000

    svnimporter: adding tags to revision 2953
    git-svn-id: svn://127.0.0.1/mai@2954 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/task_0112015/install/temp_undo_defect_edit_alter_ddl.sql
@@ -0,0 +1,17 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/mai/install/temp_undo_defect_edit_alter_ddl.sql-arc   1.1   Jun 22 2012 09:49:26   Steve.Cooper  $
+--       Module Name      : $Workfile:   temp_undo_defect_edit_alter_ddl.sql  $
+--       Date into PVCS   : $Date:   Jun 22 2012 09:49:26  $
+--       Date fetched Out : $Modtime:   Jun 22 2012 09:47:10  $
+--       PVCS Version     : $Revision:   1.1  $
+--
+--------------------------------------------------------------------------------
+--   Copyright (c) 2012 Bentley Systems Incorporated.
+--------------------------------------------------------------------------------
+--
+Alter Table Temp_Undo_Defect_Edit 
+Modify Def_X_Sect Varchar2(4)
+/
