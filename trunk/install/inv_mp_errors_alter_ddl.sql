9d12259b3eabb791feae68620f432713f75036a5
Steve.Cooper <Steve.Cooper@521c773d-b55a-d943-8648-67ffa5d26747>
2012-06-22 08:49:28 +0000

    Added Pvcs tags
    git-svn-id: svn://127.0.0.1/mai@2955 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/install/inv_mp_errors_alter_ddl.sql
+++ trunk/install/inv_mp_errors_alter_ddl.sql
@@ -1,3 +1,18 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/mai/install/inv_mp_errors_alter_ddl.sql-arc   1.1   Jun 22 2012 09:49:28   Steve.Cooper  $
+--       Module Name      : $Workfile:   inv_mp_errors_alter_ddl.sql  $
+--       Date into PVCS   : $Date:   Jun 22 2012 09:49:28  $
+--       Date fetched Out : $Modtime:   Jun 22 2012 09:47:58  $
+--       PVCS Version     : $Revision:   1.1  $
+--
+--------------------------------------------------------------------------------
+--   Copyright (c) 2012 Bentley Systems Incorporated.
+--------------------------------------------------------------------------------
+--
+
 Alter Table Inv_Mp_Errors 
 Modify Ime_Xsp Varchar2(4)
 /
--- trunk/install/mai_4520_ddl.sql
+++ trunk/install/mai_4520_ddl.sql
@@ -1,3 +1,18 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/mai/install/mai_4520_ddl.sql-arc   1.1   Jun 22 2012 09:49:28   Steve.Cooper  $
+--       Module Name      : $Workfile:   mai_4520_ddl.sql  $
+--       Date into PVCS   : $Date:   Jun 22 2012 09:49:28  $
+--       Date fetched Out : $Modtime:   Jun 22 2012 09:47:42  $
+--       PVCS Version     : $Revision:   1.1  $
+--
+--------------------------------------------------------------------------------
+--   Copyright (c) 2012 Bentley Systems Incorporated.
+--------------------------------------------------------------------------------
+--
+
 SET TERM ON
 PROMPT defects_alter_ddl.sql
 SET TERM OFF
