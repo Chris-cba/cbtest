dc371bef2fa411c4ec25275e552052f07ae4efe1
Rob.Coupe <Rob.Coupe@521c773d-b55a-d943-8648-67ffa5d26747>
2012-11-14 15:00:08 +0000

    svnimporter: adding tags to revision 3030
    git-svn-id: svn://127.0.0.1/mai@3031 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/nai_4600_fix3/install/mai_4600_fix3.sql
@@ -0,0 +1,135 @@
+--
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/mai/install/mai_4600_fix3.sql-arc   1.0   Nov 14 2012 15:00:08   Rob.Coupe  $
+--       Module Name      : $Workfile:   mai_4600_fix3.sql  $
+--       Date into SCCS   : $Date:   Nov 14 2012 15:00:08  $
+--       Date fetched Out : $Modtime:   Nov 14 2012 14:55:58  $
+--       SCCS Version     : $Revision:   1.0  $
+--
+-----------------------------------------------------------------------------
+--    Copyright (c) Bentley Systems 2012
+-----------------------------------------------------------------------------
+--
+
+set echo off
+set linesize 120
+set heading off
+set feedback off
+--
+-- Grab date/time to append to log file name
+--
+undefine log_extension
+col      log_extension new_value log_extension noprint
+set term off
+select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+/
+set term on
+--
+--------------------------------------------------------------------------------
+--
+--
+-- Spool to Logfile
+--
+define logfile1='mai_4600_fix3_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+select 'Fix Date ' || to_char(sysdate, 'DD-MON-YYYY HH24:MI:SS') from dual;
+
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+--
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET','MAI');
+
+WHENEVER SQLERROR EXIT
+
+BEGIN
+ --
+ -- Check that the user isn't sys or system
+ --
+ IF USER IN ('SYS','SYSTEM')
+ THEN
+   RAISE_APPLICATION_ERROR(-20000,'You cannot install this product as ' || USER);
+ END IF;
+
+ --
+ -- Check that HIG has been installed @ v4.6.0.0
+ --
+ hig2.product_exists_at_version (p_product        => 'MAI'
+                                ,p_VERSION        => '4.6.0.0'
+                                );
+
+END;
+/
+WHENEVER SQLERROR CONTINUE
+--
+--------------------------------------------------------------------------------
+-- Package Headers
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT hig_process_security.pkh
+SET TERM OFF
+--
+SET FEEDBACK ON
+start hig_process_security.pkh
+SET FEEDBACK OFF
+
+
+--------------------------------------------------------------------------------
+-- Package Bodies
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT hig_process_security.pkw
+SET TERM OFF
+--
+SET FEEDBACK ON
+start hig_process_security.pkw
+SET FEEDBACK OFF
+--
+--------------------------------------------------------------------------------
+-- Synonyms for package
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT Creating synonym for hig_process_security
+SET TERM OFF
+--
+SET FEEDBACK ON
+begin
+  nm3ddl.create_synonym_for_object('HIG_PROCESS_SECURITY');
+end;  
+/
+SET FEEDBACK OFF
+--
+--------------------------------------------------------------------------------
+-- Apply the security policies
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT Creating security policies
+SET TERM OFF
+--
+SET FEEDBACK ON
+start add_process_policy.sql
+SET FEEDBACK OFF
+
+--------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT log_mai_4600_fix3.sql 
+--
+SET FEEDBACK ON
+start log_mai_4600_fix3.sql
+SET FEEDBACK OFF
+SPOOL OFF
+EXIT
+--
+--
