ef1202d54da9ca6a06fe2d5a5ec535613b013843
Mike.Huitson <Mike.Huitson@521c773d-b55a-d943-8648-67ffa5d26747>
2012-12-05 10:43:22 +0000

    Task 0112303
    Convert 4 character asset codes to 2 characters before processing the file.
    Tidied up lots of compiler warnings.
    
    git-svn-id: svn://127.0.0.1/mai@3112 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/admin/c/mai2100c/r1validz.h
+++ trunk/admin/c/mai2100c/r1validz.h
@@ -1,130 +1,84 @@
-
 /* SCCS ID keywords, do not remove */
 /* "@(#)r1validz.h	1.1 09/08/03"                       */
 
-/* r1validc.h
+/* PVCS ID keywords, do not remove      */
+/* "$Workfile:   r1validz.h  $ $Revision:   2.2  $ $Modtime:   Dec 04 2012 20:18:42  $" */
 
+/* r1validc.h
 contains C functions for checking the format of husky hunter Z load records
- 
 */
-                                                              
 
 /* Z Validation
-
 function to validate the format of Z type records
-
 parameters passed : Z type record text, sequence no.
-
 return value : BOOLEAN passed or failed
-
 external action : output to stdout appropriate error message.
                   and line number . via global string err_msg. 
-
 Assumed Z card from Husky Hunter
-
 number of B cards : 4 digits right justified and packed with spaces
-
 number of C cards : 4 digits right justified and packed with spaces
-
 */
+BOOLEAN countvl();
 
 BOOLEAN z_validation(rtext,lineno)
+  /* z type record text */
+  char *rtext;
+  /* line number of record */
+  long int lineno;
+  {
+  char text[200],*strs[50];
 
-/* z type record text */
-char *rtext;
+  /* make copy of record text to allow protection of record text */
+  strcpy(text,rtext);
 
-/* line number of record */
-long int lineno;
+  getwstrs(text,strs);
 
-{ /* z_validation start brace*/
+  /* b card count */
+  countvl(strs[0],lineno);
 
-char text[200],*strs[50];
-
-int noargs,i;
-
-/* make copy of record text to allow protection of record text */
-
-strcpy(text,rtext);
-
-getwstrs(text,strs);
-
-/* b card count */
-countvl(strs[0],lineno);
-
-/*c card count */
-countvl(strs[1],lineno);
-
+  /*c card count */
+  countvl(strs[1],lineno);
  
-return(TRUE);
-
-} /* end brace z_validation */
-
+  return(TRUE);
+  }
 
 /* countvl
-
 function to validate the format of count fields from Z type records
-
 parameters passed : count field text. appropriate line number
-
 return value : BOOLEAN passed or failed
-
 validates for numeric or ' ' right justified 
-
 4 characters
-
 external action : output to stdout appropriate error message.
                   and line number . via global string err_msg. 
-
 */
+BOOLEAN countvl(count,lineno)
+  /* count field text */
+  char *count;           
+  /* line number */
+  long int lineno;
+  {
+  int i;
+  int min_chars = 4;
+  BOOLEAN num = FALSE;
 
-countvl(count,lineno)
+  if (strlen(count) < min_chars)
+    {
+    sprintf(err_msg,"ERROR: Z rec count - Less than 4 characters BPR-8046");
+    format_err(lineno,3);
+    }   
 
-/* count field text */
-char *count;           
+  for (i=0;i < strlen(count);++i)
+    {
+    if (isdigit(count[i])||((count[i] == ' ')&&(num==FALSE)&&(i!=3)))
+      {
+      num = isdigit(count[i])?TRUE:FALSE;
+      } 
+    else 
+      {
+      strcpy(err_msg,"ERROR: Counts - Non numeric or embedded space BPR-8047");
+      format_err(lineno,3);
+      }      
+    }
 
-/* line number */
-long int lineno;
-
-{ /*countvl start brace */
-
-int i;
-int max_chars = 6;
-int min_chars = 4;
-
-BOOLEAN num;
-
-num = FALSE;    
-
-/*  Max check removed, no longer required  
-if (strlen(count) > max_chars)
-{ sprintf(err_msg,
-"ERROR: Z rec count - More than 4 characters BPR-8045");
-format_err(lineno,3);
-}   
-*/
-if (strlen(count) < min_chars)
-{ sprintf(err_msg,
-"ERROR: Z rec count - Less than 4 characters BPR-8046");
-format_err(lineno,3);
-}   
-
-for (i=0;i < strlen(count);++i)
-{
-
-if (
-isdigit(count[i])
-||
-((count[i] == ' ')&&(num==FALSE)&&(i!=3))
-   ) /* end of IF */
-  { num = isdigit(count[i])?TRUE:FALSE;} 
-else 
-{ strcpy(err_msg,
- "ERROR: Counts - Non numeric or embedded space BPR-8047");
-format_err(lineno,3);
-}      
-
-} /* end brace FOR */
-
-return(TRUE);                        
-
-} /* end brace countvl */
+  return(TRUE);                        
+  } /* end brace countvl */
