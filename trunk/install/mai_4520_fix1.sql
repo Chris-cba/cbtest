249565d5269af9911f95423c42fd9314574d9463
Mike.Huitson <Mike.Huitson@521c773d-b55a-d943-8648-67ffa5d26747>
2012-12-07 12:46:44 +0000

    Initial Revision
    git-svn-id: svn://127.0.0.1/mai@3128 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ trunk/install/mai_4520_fix1.sql
@@ -0,0 +1,96 @@
+--
+--------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //vm_latest/archives/mai/install/mai_4520_fix1.sql-arc   3.0   Dec 07 2012 12:46:44   Mike.Huitson  $
+--       Module Name      : $Workfile:   mai_4520_fix1.sql  $
+--       Date into PVCS   : $Date:   Dec 07 2012 12:46:44  $
+--       Date fetched Out : $Modtime:   Dec 06 2012 17:38:10  $
+--       PVCS Version     : $Revision:   3.0  $
+--
+--------------------------------------------------------------------------------
+--   Copyright (c) exor corporation ltd, 2012
+--------------------------------------------------------------------------------
+--
+set echo off
+set linesize 120
+set heading off
+set feedback off
+--
+-- Grab date/time to append to log file name
+--
+undefine log_extension
+col      log_extension new_value log_extension noprint
+set term off
+select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+/
+set term on
+--
+--------------------------------------------------------------------------------
+--
+--
+-- Spool to Logfile
+--
+define logfile1='mai_4520_fix1_1_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+select 'Fix Date ' || to_char(sysdate, 'DD-MON-YYYY HH24:MI:SS') from dual;
+
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+--
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET','MAI');
+
+WHENEVER SQLERROR EXIT
+
+BEGIN
+  --
+  -- Check that the user isn't sys or system
+  --
+  IF USER IN ('SYS','SYSTEM')
+  THEN
+    RAISE_APPLICATION_ERROR(-20000,'You cannot install this product as ' || USER);
+  END IF;
+
+  --
+  -- Check that MAI has been installed @ v4.5.2.0
+  --
+  hig2.product_exists_at_version (p_product        => 'MAI'
+                                 ,p_VERSION        => '4.5.2.0'
+                                 );
+
+END;
+/
+
+WHENEVER SQLERROR CONTINUE
+--
+--------------------------------------------------------------------------------
+-- Package Bodies
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT mai_inspection_api.pkw
+SET TERM OFF
+--
+SET FEEDBACK ON
+start mai_inspection_api.pkw
+SET FEEDBACK OFF
+--
+--------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT log_mai_4520_fix1.sql 
+--
+SET FEEDBACK ON
+start log_mai_4520_fix1.sql
+SET FEEDBACK OFF
+SPOOL OFF
+EXIT
+--
+--
