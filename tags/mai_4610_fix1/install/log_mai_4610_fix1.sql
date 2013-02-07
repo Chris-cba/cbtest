4aadcc5a5ac782fd2b45e4f86aa41bac63c76491
Mike.Huitson <Mike.Huitson@521c773d-b55a-d943-8648-67ffa5d26747>
2013-02-07 16:05:32 +0000

    svnimporter: adding tags to revision 3207
    git-svn-id: svn://127.0.0.1/mai@3208 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/mai_4610_fix1/install/log_mai_4610_fix1.sql
@@ -0,0 +1,13 @@
+BEGIN
+--
+  hig2.upgrade(p_product        => 'MAI'
+              ,p_upgrade_script => 'log_mai_4610_fix1.sql'
+              ,p_remarks        => 'MAI 4610 FIX 1'
+              ,p_to_version     => Null);
+--
+  commit;
+--
+EXCEPTION
+  WHEN others THEN Null;
+END;
+/
