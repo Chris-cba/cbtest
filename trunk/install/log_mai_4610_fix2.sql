10d8a2713f0cd0618a3f48b99a30e26fcb4ad088
Chris.Baugh <Chris.Baugh@521c773d-b55a-d943-8648-67ffa5d26747>
2013-04-02 08:45:38 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/mai@3252 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ trunk/install/log_mai_4610_fix2.sql
@@ -0,0 +1,13 @@
+BEGIN
+--
+  hig2.upgrade(p_product        => 'MAI'
+              ,p_upgrade_script => 'log_mai_4610_fix2.sql'
+              ,p_remarks        => 'MAI 4610 FIX 2'
+              ,p_to_version     => Null);
+--
+  commit;
+--
+EXCEPTION
+  WHEN others THEN Null;
+END;
+/
