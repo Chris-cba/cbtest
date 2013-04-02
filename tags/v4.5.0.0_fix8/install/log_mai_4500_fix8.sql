335178152c2846613ef505c3ea769e81f3e1bb38
Chris.Baugh <Chris.Baugh@521c773d-b55a-d943-8648-67ffa5d26747>
2013-04-02 08:43:48 +0000

    svnimporter: adding tags to revision 3250
    git-svn-id: svn://127.0.0.1/mai@3251 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/v4.5.0.0_fix8/install/log_mai_4500_fix8.sql
@@ -0,0 +1,13 @@
+BEGIN
+--
+  hig2.upgrade(p_product        => 'MAI'
+              ,p_upgrade_script => 'log_mai_4500_fix8.sql'
+              ,p_remarks        => 'MAI 4500 FIX 8'
+              ,p_to_version     => Null);
+--
+  commit;
+--
+EXCEPTION
+  WHEN others THEN Null;
+END;
+/
