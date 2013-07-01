3983d03c59331ab3a037b9ab80e5f5c897fe26f0
James.Wadsworth <James.Wadsworth@521c773d-b55a-d943-8648-67ffa5d26747>
2013-07-01 15:02:22 +0000

    S-95677: Added Bentley copyright notice to the file.
    git-svn-id: svn://127.0.0.1/mai@3387 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/install/mai40_mai4010.sql
+++ trunk/install/mai40_mai4010.sql
@@ -3,17 +3,17 @@
 --
 --   SCCS Identifiers :-
 --
---       sccsid           : $Header:   //vm_latest/archives/mai/install/mai40_mai4010.sql-arc   2.0   Jun 13 2007 16:32:36   smarshall  $
+--       sccsid           : $Header:   //vm_latest/archives/mai/install/mai40_mai4010.sql-arc   2.1   Jul 01 2013 16:02:22   James.Wadsworth  $
 --       Module Name      : $Workfile:   mai40_mai4010.sql  $
---       Date into SCCS   : $Date:   Jun 13 2007 16:32:36  $
---       Date fetched Out : $Modtime:   Jun 13 2007 16:31:50  $
---       SCCS Version     : $Revision:   2.0  $
+--       Date into SCCS   : $Date:   Jul 01 2013 16:02:22  $
+--       Date fetched Out : $Modtime:   Jul 01 2013 14:32:36  $
+--       SCCS Version     : $Revision:   2.1  $
 --       Based on SCCS Version     : 1.2
 --
 --   Product upgrade script
 --
 -----------------------------------------------------------------------------
---	Copyright (c) exor corporation ltd, 2007
+--   Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved.
 -----------------------------------------------------------------------------
 undefine exor_base
 undefine run_file
