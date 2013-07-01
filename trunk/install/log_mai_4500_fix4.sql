da0cf033265176e28f2a1c6a6393f4f2a3f82ddb
James.Wadsworth <James.Wadsworth@521c773d-b55a-d943-8648-67ffa5d26747>
2013-07-01 15:16:22 +0000

    S-95677: Added Bentley copyright notice to the file.
    git-svn-id: svn://127.0.0.1/mai@3411 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/install/log_mai_4500_fix4.sql
+++ trunk/install/log_mai_4500_fix4.sql
@@ -1,3 +1,14 @@
+-------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //vm_latest/archives/mai/install/log_mai_4500_fix4.sql-arc   3.1   Jul 01 2013 16:16:22   James.Wadsworth  $
+--       Module Name      : $Workfile:   log_mai_4500_fix4.sql  $
+--       Date into PVCS   : $Date:   Jul 01 2013 16:16:22  $
+--       Date fetched Out : $Modtime:   Jul 01 2013 16:09:46  $
+--       Version          : $Revision:   3.1  $
+------------------------------------------------------------------
+--   Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved.
+------------------------------------------------------------------
 BEGIN
 --
   hig2.upgrade(p_product        => 'MAI'
