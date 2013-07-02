39bae2ea95c9fe99dccf872f6ea68c3ebd53fae6
James.Wadsworth <James.Wadsworth@521c773d-b55a-d943-8648-67ffa5d26747>
2013-07-02 09:30:40 +0000

    svnimporter: adding tags to revision 3461
    git-svn-id: svn://127.0.0.1/mai@3462 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/build_1_4700/admin/c/mai2210/r7rep.c
@@ -0,0 +1,37 @@
+
+/* SCCS ID keywords, do not remove */
+/* "@(#)r7rep.c	1.1 09/16/03"                       */
+/* Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved. */
+
+/************************************************************ 
+* Standard I/O definition and rmms header file.             *
+************************************************************/ 
+#include <stdio.h>
+#include "rmms.h"
+
+
+
+
+FILE *rep_secs()
+{
+void set_sec_qry();
+FILE *fp,*def_rs_rep();
+int id,per_defects,tot_defects,tot_rs;    
+char dtp_id[16];
+short int fin_sec;
+
+  set_sec_qry();
+
+
+  do
+  {
+
+    fin_sec = fch_sec(&id,dtp_id,&per_defects,&tot_defects,&tot_rs);   
+    fp=def_rs_rep(id,dtp_id,tot_defects,tot_rs,per_defects);    
+
+  } while (fin_sec != -1) ;
+
+return(fp);
+}
+
+
