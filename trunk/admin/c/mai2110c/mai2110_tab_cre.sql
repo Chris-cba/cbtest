f75178cc3e459a2332df88ad7398fb51272d77cd
James.Wadsworth <James.Wadsworth@521c773d-b55a-d943-8648-67ffa5d26747>
2013-07-02 09:29:04 +0000

    S-95677: Added Bentley copyright notice to the file.
    git-svn-id: svn://127.0.0.1/mai@3455 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/admin/c/mai2110c/mai2110_tab_cre.sql
+++ trunk/admin/c/mai2110c/mai2110_tab_cre.sql
@@ -1,10 +1,14 @@
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/mai/admin/c/mai2110c/mai2110_tab_cre.sql-arc   2.2   Sep 03 2009 13:33:46   mhuitson  $
+--       pvcsid           : $Header:   //vm_latest/archives/mai/admin/c/mai2110c/mai2110_tab_cre.sql-arc   2.3   Jul 02 2013 10:29:04   James.Wadsworth  $
 --       Module Name      : $Workfile:   mai2110_tab_cre.sql  $
---       Date into PVCS   : $Date:   Sep 03 2009 13:33:46  $
---       Date fetched Out : $Modtime:   Sep 03 2009 13:33:02  $
---       PVCS Version     : $Revision:   2.2  $
+--       Date into PVCS   : $Date:   Jul 02 2013 10:29:04  $
+--       Date fetched Out : $Modtime:   Jul 02 2013 09:54:14  $
+--       PVCS Version     : $Revision:   2.3  $
+--
+------------------------------------------------------------------
+--   Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved.
+------------------------------------------------------------------
 --
 CREATE TABLE hhinv_temp_7883
             (he_id     NUMBER(38,0)
--- trunk/admin/c/mai2110c/mai2110c.mk
+++ trunk/admin/c/mai2110c/mai2110c.mk
@@ -7,6 +7,8 @@
 #       Date fetched Out : 07/06/07 11:27:00
 #       SCCS Version     : 1.1# 
 #
+#  Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved.
+#
 # ***********************************************************************
 # 
 # To make this module :
