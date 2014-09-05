7bc86217b41aab6b63809d4911dd4ac2c21dd544
Peter.Bibby <Peter.Bibby@521c773d-b55a-d943-8648-67ffa5d26747>
2014-09-05 09:25:12 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/mai@3552 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ trunk/install/mai_4700_fix3.sql
@@ -0,0 +1,135 @@
+--------------------------------------------------------------------------------
+--   Copyright (c) 2014 Bentley Systems Incorporated. All rights reserved
+--------------------------------------------------------------------------------
+--
+set echo off
+set linesize 120
+set heading off
+set feedback off
+--
+-- Grab date/time to append to log file name
+--
+undefine log_extension
+col      log_extension new_value log_extension noprint
+set term off
+select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+/
+set term on
+--
+--------------------------------------------------------------------------------
+--
+--
+-- Spool to Logfile
+--
+define logfile1='mai_4700_fix3_1_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+select 'Fix Date ' || to_char(sysdate, 'DD-MON-YYYY HH24:MI:SS') from dual;
+
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+--
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG','NET','MAI');
+
+WHENEVER SQLERROR EXIT
+
+BEGIN
+  --
+  -- Check that the user isn't sys or system
+  --
+  IF USER IN ('SYS','SYSTEM')
+  THEN
+    RAISE_APPLICATION_ERROR(-20000,'You cannot install this product as ' || USER);
+  END IF;
+
+  --
+  -- Check that MAI has been installed @ v4.7.0.0
+  --
+  hig2.product_exists_at_version (p_product        => 'MAI'
+                                 ,p_VERSION        => '4.7.0.0'
+                                 );
+
+END;
+/
+
+WHENEVER SQLERROR CONTINUE
+--
+--------------------------------------------------------------------------------
+-- mai_inspection_api.sql
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT mai_inspection_api.pkw
+SET TERM OFF
+--
+SET FEEDBACK ON
+start mai_inspection_api.pkw
+SET FEEDBACK OFF
+--
+--------------------------------------------------------------------------------
+-- Product Option
+--------------------------------------------------------------------------------
+SET TERM ON 
+PROMPT Adding new product option DEFDOCCAT into HIG_OPTION_LIST
+SET TERM OFF
+--
+
+INSERT INTO HIG_OPTION_LIST 
+           (HOL_ID,
+            HOL_PRODUCT,
+            HOL_NAME,
+            HOL_REMARKS,
+            HOL_DOMAIN,
+            HOL_DATATYPE,
+            HOL_MIXED_CASE,
+            HOL_USER_OPTION,
+            HOL_MAX_LENGTH)
+SELECT 'DEFDOCCAT',
+             'MAI',
+             'Default Document Category',
+             'Contains the default Document Category for Defect document attachments loaded via Maintenace Inspection Loader.',
+             '',
+             'VARCHAR2',
+             'N',
+             'Y',
+              2000
+FROM DUAL
+WHERE NOT EXISTS (SELECT 1
+                  FROM HIG_OPTION_LIST
+                  WHERE HOL_ID = 'DEFDOCCAT'
+                  )
+/
+
+--
+SET TERM ON 
+PROMPT Adding DEFDOCTYPE,DEFDOCLOCN as User Options
+SET TERM OFF
+--
+UPDATE HIG_OPTION_LIST
+SET HOL_USER_OPTION ='Y'
+WHERE HOL_ID in ('DEFDOCTYPE','DEFDOCLOCN')
+/
+COMMIT
+/
+--------------------------------------------------------------------------------
+-- Views
+--------------------------------------------------------------------------------
+--
+----------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT log_mai_4700_fix3.sql 
+--
+SET FEEDBACK ON
+start log_mai_4700_fix3.sql
+SET FEEDBACK OFF
+SPOOL OFF
+EXIT
+--
+--
