c378171c7e5288755c4f01f165b792b6689c13ee
Mike.Huitson <Mike.Huitson@521c773d-b55a-d943-8648-67ffa5d26747>
2014-12-02 14:34:40 +0000

    Updated for build 2 of the fix.
    git-svn-id: svn://127.0.0.1/mai@3582 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/install/log_mai_4700_fix4.sql
+++ trunk/install/log_mai_4700_fix4.sql
@@ -1,11 +1,11 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/mai/install/log_mai_4700_fix4.sql-arc   3.0   Oct 23 2014 15:16:30   Mike.Huitson  $
+--       PVCS id          : $Header:   //vm_latest/archives/mai/install/log_mai_4700_fix4.sql-arc   3.1   Dec 02 2014 14:34:40   Mike.Huitson  $
 --       Module Name      : $Workfile:   log_mai_4700_fix4.sql  $
---       Date into PVCS   : $Date:   Oct 23 2014 15:16:30  $
---       Date fetched Out : $Modtime:   Oct 23 2014 15:11:28  $
---       Version          : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Dec 02 2014 14:34:40  $
+--       Date fetched Out : $Modtime:   Dec 02 2014 14:33:18  $
+--       Version          : $Revision:   3.1  $
 ------------------------------------------------------------------
 --   Copyright (c) 2014 Bentley Systems Incorporated. All rights reserved.
 ------------------------------------------------------------------
@@ -13,7 +13,7 @@ BEGIN
 --
   hig2.upgrade(p_product        => 'MAI'
               ,p_upgrade_script => 'log_mai_4700_fix4.sql'
-              ,p_remarks        => 'MAI 4700 FIX 4'
+              ,p_remarks        => 'MAI 4700 FIX 4 (Build 2)'
               ,p_to_version     => Null);
 --
   commit;
