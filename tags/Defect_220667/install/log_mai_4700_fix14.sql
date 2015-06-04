534917da5319d1939ffc2297aeafa3379dbdf53d
Upendra.Hukeri <Upendra.Hukeri@521c773d-b55a-d943-8648-67ffa5d26747>
2015-06-04 04:15:34 +0000

    svnimporter: adding tags to revision 3643
    git-svn-id: svn://127.0.0.1/mai@3644 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/Defect_220667/install/log_mai_4700_fix14.sql
@@ -0,0 +1,17 @@
+BEGIN
+	--
+	hig2.upgrade(p_product        => 'MAI'
+				,p_upgrade_script => 'log_mai_4700_fix14.sql'
+				,p_remarks        => 'MAI 4700 FIX 14'
+				,p_to_version     => NULL
+				);
+	--
+	COMMIT;
+	--
+EXCEPTION 
+	WHEN OTHERS THEN
+	--
+		NULL;
+	--
+END;
+/
