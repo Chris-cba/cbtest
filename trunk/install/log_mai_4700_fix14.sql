2139faff1c63adb8f42da3fcb2ac96c69931ee18
Upendra.Hukeri <Upendra.Hukeri@521c773d-b55a-d943-8648-67ffa5d26747>
2015-12-11 08:49:38 +0000

    Added (Build 2) to the remarks field.
    git-svn-id: svn://127.0.0.1/mai@3693 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/install/log_mai_4700_fix14.sql
+++ trunk/install/log_mai_4700_fix14.sql
@@ -2,7 +2,7 @@ BEGIN
 	--
 	hig2.upgrade(p_product        => 'MAI'
 				,p_upgrade_script => 'log_mai_4700_fix14.sql'
-				,p_remarks        => 'MAI 4700 FIX 14'
+				,p_remarks        => 'MAI 4700 FIX 14 (Build 2)'
 				,p_to_version     => NULL
 				);
 	--
