59332d8e6fb37d9f0a6ef2fb57a14313b639a1a1
Upendra.Hukeri <Upendra.Hukeri@521c773d-b55a-d943-8648-67ffa5d26747>
2015-12-14 06:54:34 +0000

    svnimporter: adding tags to revision 3697
    git-svn-id: svn://127.0.0.1/mai@3698 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/exmai04070002en_updt14/install/mai_4700_fix14.sql
@@ -0,0 +1,110 @@
+----------------------------------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //new_vm_latest/archives/mai/install/mai_4700_fix14.sql-arc   1.1   Dec 14 2015 06:54:34   Upendra.Hukeri  $
+--       Module Name      : $Workfile:   mai_4700_fix14.sql  $ 
+--       Date into PVCS   : $Date:   Dec 14 2015 06:54:34  $
+--       Date fetched Out : $Modtime:   Dec 14 2015 06:54:00  $
+--       Version     	  : $Revision:   1.1  $
+--
+----------------------------------------------------------------------------------------------------
+--   Copyright (c) 2015 Bentley Systems Incorporated. All rights reserved.
+----------------------------------------------------------------------------------------------------
+--
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+--
+-- Grab date/time to append to log file name
+--
+UNDEFINE log_extension
+COL      log_extension NEW_VALUE log_extension NOPRINT
+SET TERM OFF
+SELECT  TO_CHAR(SYSDATE, 'DDMONYYYY_HH24MISS') || '.LOG' log_extension FROM DUAL
+/
+SET TERM ON
+--
+--------------------------------------------------------------------------------
+-- Spool to Logfile
+--------------------------------------------------------------------------------
+--
+DEFINE logfile1='mai_4700_fix14_&log_extension'
+spool &logfile1
+--
+--------------------------------------------------------------------------------
+--
+SELECT 'Fix Date ' || TO_CHAR(SYSDATE, 'DD-MON-YYYY HH24:MI:SS') FROM DUAL;
+
+SELECT 'Install Running on ' || LOWER(USER || '@' || instance_name || '.' || host_name) || ' - DB ver : ' || version
+FROM v$instance;
+--
+SELECT 'Current version of ' || hpr_product || ' ' || hpr_version
+FROM hig_products
+WHERE hpr_product IN ('HIG', 'NET', 'MAI');
+--
+--------------------------------------------------------------------------------
+-- 	Check(s)
+--------------------------------------------------------------------------------
+--
+WHENEVER SQLERROR EXIT
+--
+DECLARE
+	--
+	CURSOR c1 is
+		SELECT 'Y'
+		FROM   hig_upgrades
+		WHERE  hup_product = 'NET'
+		AND    remarks = 'NET 4700 FIX 23 (Build 2)';
+	--
+	l_dummy_c1 VARCHAR2(1);
+	--
+BEGIN
+	--
+	-- Check that the user isn't SYS or SYSTEM
+	--
+	IF USER IN ('SYS', 'SYSTEM') THEN
+		RAISE_APPLICATION_ERROR(-20000, 'You cannot install this product as ' || USER);
+	END IF;
+	--
+	-- Check that MAI has been installed @ v4.7.0.x
+	--
+	hig2.product_exists_at_version  (p_product        => 'MAI'
+									,p_VERSION        => '4.7.0.3'
+									);
+	--
+	-- 	Check that NET 4700 FIX 23 (Build 2) has already been applied
+	--
+	OPEN  c1;
+	FETCH c1 INTO l_dummy_c1;
+	CLOSE c1;
+	--
+	IF l_dummy_c1 IS NULL THEN
+		RAISE_APPLICATION_ERROR(-20001,'NET 4700 FIX 23 (Build 2) must be applied before proceeding - contact exor support for further information');
+	END IF;
+	--
+END;
+/
+--
+WHENEVER SQLERROR CONTINUE
+--
+--------------------------------------------------------------------------------
+-- Update hig_upgrades with fix ID
+--------------------------------------------------------------------------------
+--
+SET TERM ON 
+PROMPT log_mai_4700_fix14.sql
+SET TERM OFF
+--
+SET FEEDBACK ON
+START log_mai_4700_fix14.sql
+SET FEEDBACK OFF
+--
+SPOOL OFF
+--
+EXIT
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   END OF SCRIPT   *******************
+---------------------------------------------------------------------------------------------------
+--
