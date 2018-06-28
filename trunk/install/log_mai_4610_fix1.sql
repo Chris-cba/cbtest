e76f9de00bf98d6aa38456624be40af494029872
Gaurav.Gaurkar <Gaurav.Gaurkar@521c773d-b55a-d943-8648-67ffa5d26747>
2018-06-28 06:41:46 +0000

    Modified copyright date to 2018 for 48 build
    git-svn-id: svn://127.0.0.1/mai@3792 521c773d-b55a-d943-8648-67ffa5d26747

--- trunk/install/log_mai_4610_fix1.sql
+++ trunk/install/log_mai_4610_fix1.sql
@@ -1,11 +1,11 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/mai/install/log_mai_4610_fix1.sql-arc   3.1   Jul 01 2013 16:16:24   James.Wadsworth  $
+--       PVCS id          : $Header:   //new_vm_latest/archives/mai/install/log_mai_4610_fix1.sql-arc   3.2   Jun 28 2018 07:41:46   Gaurav.Gaurkar  $
 --       Module Name      : $Workfile:   log_mai_4610_fix1.sql  $
---       Date into PVCS   : $Date:   Jul 01 2013 16:16:24  $
+--       Date into PVCS   : $Date:   Jun 28 2018 07:41:46  $
 --       Date fetched Out : $Modtime:   Jul 01 2013 16:10:44  $
---       Version          : $Revision:   3.1  $
+--       Version          : $Revision:   3.2  $
 ------------------------------------------------------------------
 --   Copyright (c) 2013 Bentley Systems Incorporated. All rights reserved.
 ------------------------------------------------------------------
