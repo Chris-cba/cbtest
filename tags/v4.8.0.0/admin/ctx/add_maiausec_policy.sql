e14b8696dfbb6ad7fe2f838d10d63e60c440a584
Chris.Baugh <Chris.Baugh@521c773d-b55a-d943-8648-67ffa5d26747>
2019-09-27 12:20:22 +0000

    svnimporter: adding tags to revision 3845
    git-svn-id: svn://127.0.0.1/mai@3846 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/v4.8.0.0/admin/ctx/add_maiausec_policy.sql
@@ -0,0 +1,123 @@
+--   PVCS Identifiers :-
+--
+--       sccsid           : $Header:   //new_vm_latest/archives/mai/admin/ctx/add_maiausec_policy.sql-arc   1.9   Sep 27 2019 13:20:22   Chris.Baugh  $
+--       Module Name      : $Workfile:   add_maiausec_policy.sql  $
+--       Date into SCCS   : $Date:   Sep 27 2019 13:20:22  $
+--       Date fetched Out : $Modtime:   Sep 27 2019 13:16:34  $
+--       SCCS Version     : $Revision:   1.9  $
+--
+-----------------------------------------------------------------------------
+--    Copyright (c) 2019 Bentley Systems Incorporated. All rights reserved.
+-----------------------------------------------------------------------------
+
+
+SET SERVEROUTPUT ON SIZE 1000000
+--
+DECLARE
+--
+-----------------------------------------------------------------------------
+--   Copyright (c) 2019 Bentley Systems Incorporated. All rights reserved.
+-----------------------------------------------------------------------------
+--
+   l_tab_policy_name     nm3type.tab_varchar30;
+   l_tab_object_name     nm3type.tab_varchar30;
+   l_tab_policy_function nm3type.tab_varchar2000;
+   l_tab_statement_types nm3type.tab_varchar30;
+   l_tab_check_option    nm3type.tab_boolean;
+   l_tab_enable          nm3type.tab_boolean;
+   l_tab_static_policy   nm3type.tab_boolean;
+   --
+   c_application_owner   CONSTANT VARCHAR2(30) := Sys_Context('NM3CORE','APPLICATION_OWNER');
+--
+   l_no_fine_grained_security EXCEPTION;
+   PRAGMA EXCEPTION_INIT (l_no_fine_grained_security,-439);
+   --
+   PROCEDURE add_policy (p_policy_name     VARCHAR2
+                        ,p_object_name     VARCHAR2
+                        ,p_policy_function VARCHAR2
+                        ,p_statement_types VARCHAR2
+                        ,p_check_option    BOOLEAN DEFAULT TRUE
+                        ,p_enable          BOOLEAN DEFAULT TRUE
+                        ,p_static_policy   BOOLEAN DEFAULT FALSE
+                        ) IS
+      i CONSTANT PLS_INTEGER := l_tab_policy_name.COUNT + 1;
+   BEGIN
+      l_tab_policy_name(i)     := p_policy_name;
+      l_tab_object_name(i)     := p_object_name;
+      l_tab_policy_function(i) := p_policy_function;
+      l_tab_statement_types(i) := p_statement_types;
+      l_tab_check_option(i)    := p_check_option;
+      l_tab_enable(i)          := p_enable;
+      l_tab_static_policy(i)   := p_static_policy;
+   END add_policy;
+   --
+BEGIN
+--   add_policy (p_policy_name     =>
+--              ,p_object_name     =>
+--              ,p_policy_function =>
+--              ,p_statement_types =>
+--              );
+--
+-- SELECT policies for inventory
+--
+add_policy ( p_policy_name    => 'DSR_6_AU_READ'            ,p_object_name => 'DEF_SUPERSEDING_RULES'            ,p_policy_function => 'MAIAUSEC.DSR_6_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'BUD_12_AU_READ'            ,p_object_name => 'BUDGETS'            ,p_policy_function => 'MAIAUSEC.BUD_12_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'ICB_13_AU_READ'            ,p_object_name => 'ITEM_CODE_BREAKDOWNS'            ,p_policy_function => 'MAIAUSEC.ICB_13_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'MAW_17_AU_READ'            ,p_object_name => 'MAI_AUTO_WO_RULES'            ,p_policy_function => 'MAIAUSEC.MAW_17_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'DEF_18_AU_READ'            ,p_object_name => 'DEFECTS'            ,p_policy_function => 'MAIAUSEC.DEF_18_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'ARE_20_AU_READ'            ,p_object_name => 'ACTIVITIES_REPORT'            ,p_policy_function => 'MAIAUSEC.ARE_20_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'DEF_22_AU_READ'            ,p_object_name => 'MAI_INSP_LOAD_ERROR_DEF'            ,p_policy_function => 'MAIAUSEC.DEF_22_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'WOL_23_AU_READ'            ,p_object_name => 'WORK_ORDER_LINES'            ,p_policy_function => 'MAIAUSEC.WOL_23_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'DLD_34_AU_READ'            ,p_object_name => 'DELETED_DEFECTS'            ,p_policy_function => 'MAIAUSEC.DLD_34_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'ARE_42_AU_READ'            ,p_object_name => 'MAI_INSP_LOAD_ERROR_ARE'            ,p_policy_function => 'MAIAUSEC.ARE_42_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'SCH_45_AU_READ'            ,p_object_name => 'SCHEDULE_ROADS'            ,p_policy_function => 'MAIAUSEC.SCH_45_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'MAI_46_AU_READ'            ,p_object_name => 'MAI2325_RESULTS'            ,p_policy_function => 'MAIAUSEC.MAI_46_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'REP_47_AU_READ'            ,p_object_name => 'REPAIRS'            ,p_policy_function => 'MAIAUSEC.REP_47_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'LFR_49_AU_READ'            ,p_object_name => 'LOCAL_FREQS'            ,p_policy_function => 'MAIAUSEC.LFR_49_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'SCH_61_AU_READ'            ,p_object_name => 'SCHEDULES'            ,p_policy_function => 'MAIAUSEC.SCH_61_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+add_policy ( p_policy_name    => 'REP_62_AU_READ'            ,p_object_name => 'MAI_INSP_LOAD_ERROR_REP'            ,p_policy_function => 'MAIAUSEC.REP_62_PREDICATE_READ'            ,p_statement_types => 'SELECT,INSERT,UPDATE,DELETE' ); 
+--
+--
+-- Manual ones
+--
+
+add_policy ( p_policy_name    => 'CONTRACTS_AU_READ'            ,p_object_name => 'ORG_UNITS'            ,p_policy_function => 'MAIAUSEC.CONTRACTS_PREDICATE_READ'            ,p_statement_types => 'SELECT' );
+
+--
+   FOR l_count IN 1..l_tab_policy_name.COUNT
+
+    LOOP
+      BEGIN
+         dbms_rls.add_policy
+            (object_schema   => c_application_owner
+            ,object_name     => l_tab_object_name(l_count)
+            ,policy_name     => l_tab_policy_name(l_count)
+            ,function_schema => c_application_owner
+            ,policy_function => l_tab_policy_function(l_count)
+            ,statement_types => l_tab_statement_types(l_count)
+            ,update_check    => l_tab_check_option(l_count)
+            ,enable          => l_tab_enable(l_count)
+            ,static_policy   => l_tab_static_policy(l_count)
+            );
+         dbms_output.put_line(l_count||'. '||l_tab_policy_name(l_count)||' on '||c_application_owner||'.'||l_tab_object_name(l_count)||' created');
+      EXCEPTION
+         WHEN l_no_fine_grained_security
+          THEN
+            RAISE;
+         WHEN others
+          THEN
+            dbms_output.put_line(l_count||'. ### '||l_tab_policy_name(l_count)||' on '||l_tab_object_name(l_count)||' ERROR');
+            dbms_output.put_line(SUBSTR(SQLERRM,1,255));
+      END;
+      dbms_output.put_line('--');
+   END LOOP;
+--
+EXCEPTION
+   WHEN l_no_fine_grained_security
+    THEN
+      dbms_output.put_line(SUBSTR(SQLERRM,1,255));
+END;
+/
+
+
+
