6420c0c40b1687ff61a29fc6932a3ec0b92353b4
Chris.Baugh <Chris.Baugh@521c773d-b55a-d943-8648-67ffa5d26747>
2019-09-27 14:20:16 +0000

    svnimporter: adding tags to revision 3895
    git-svn-id: svn://127.0.0.1/mai@3896 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/v4.8.0.0/admin/utl/cleaninv.sql
@@ -0,0 +1,26 @@
+REM SCCS ID Keyword, do no remove
+REM Copyright (c) 2019 Bentley Systems Incorporated. All rights reserved.
+
+define sccsid = '$Revision:   2.3  $';
+delete from hhinv_run_log;
+insert into hhinv_run_log (fst_run_date,
+                           lst_run_date,
+                           no_of_runs,
+                           run_num,
+                           run_stage,
+                           run_stat)
+values (to_date('01011901','DDMMYYYY'),
+        to_date('01011901','DDMMYYYY'),
+        0,
+        0,
+        10,
+        10);
+delete from hhinv_hold_1;
+delete from hhinv_item_err_1;
+delete from hhinv_item_err_2;
+delete from hhinv_item_err_3;
+delete from hhinv_load_1;
+delete from hhinv_load_2;
+delete from hhinv_load_3;
+delete from hhinv_odl_log;
+delete from hhinv_sect_log;
