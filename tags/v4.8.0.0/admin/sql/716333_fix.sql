787a980a25ed929d2893f4ae289d40344b75075f
Chris.Baugh <Chris.Baugh@521c773d-b55a-d943-8648-67ffa5d26747>
2019-09-27 13:41:56 +0000

    svnimporter: adding tags to revision 3881
    git-svn-id: svn://127.0.0.1/mai@3882 521c773d-b55a-d943-8648-67ffa5d26747

---
+++ tags/v4.8.0.0/admin/sql/716333_fix.sql
@@ -0,0 +1,48 @@
+-- PVCS Identifiers :-
+--
+-- pvcsid : $Header:   //new_vm_latest/archives/mai/admin/sql/716333_fix.sql-arc   3.3   Sep 27 2019 14:41:56   Chris.Baugh  $
+-- Module Name : $Workfile:   716333_fix.sql  $
+-- Date into PVCS : $Date:   Sep 27 2019 14:41:56  $
+-- Date fetched Out : $Modtime:   Sep 27 2019 14:41:26  $
+-- PVCS Version : $Revision:   3.3  $
+------------------------------------------------------------------
+--   Copyright (c) 2019 Bentley Systems Incorporated. All rights reserved.
+------------------------------------------------------------------
+
+insert into hig_option_list
+      (hol_id
+      ,hol_product
+      ,hol_name
+      ,hol_remarks
+      ,hol_domain
+      ,hol_datatype
+      ,hol_mixed_case
+      ,hol_user_option) 
+select 'XTRIFLDS'
+      ,'MAI'
+      ,'Version of Interface Files'      
+      ,'If set to 2-1-3 uses additional fields on the end of the WO and WI files. Otherwise set to NA.'
+      ,null
+      ,'VARCHAR2'
+      ,'N'
+      ,'N'
+  from dual
+ where not exists (select 1
+                     from hig_option_list
+                    where hol_id = 'XTRIFLDS')
+/
+alter trigger hov_b_iu_trg disable;
+
+insert into hig_option_values
+      (hov_id
+      ,hov_value)
+select 'XTRIFLDS'
+      ,'2-1-3'
+  from dual
+ where not exists (select 1
+                     from hig_option_values
+                    where hov_id = 'XTRIFLDS')
+/
+alter trigger hov_b_iu_trg enable;
+commit
+/
